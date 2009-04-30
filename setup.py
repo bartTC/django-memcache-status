399ac66f1f7075fedccb9e1c1b5b8a0dfd66a4d1
Martin Mahner <martin@mahner.org>
2009-04-30 11:14:03 +0200

    Added some cheesy files. Added the app to pypi.

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='django-memcache-status',
+    version='0.1',
+    description='A django application that displays the load and some other statistics about your memcached instances in the admin.',
+    long_description=open('README.rst').read(),
+    author='Martin Mahner',
+    author_email='martin@mahner.org',
+    url='http://github.com/bartTC/django-memcache-status',
+    packages=find_packages(exclude=['example_project', 'example_project.*']),
+    include_package_data=True,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+    ],
+    zip_safe=False,
+)
