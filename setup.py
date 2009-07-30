459697145d75835c273e7e6f2b5798a4b523451f
Martin Mahner <martin@mahner.org>
2009-07-30 14:56:30 +0200

    No issues, let's bump that up to v1.0.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-memcache-status',
-    version='0.9',
+    version='1.0',
     description='A django application that displays the load and some other statistics about your memcached instances in the admin.',
     long_description=open('README.rst').read(),
     author='Martin Mahner',
